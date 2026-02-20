0fc567bedc8f16e01472c19440abeb608377fd3d
drg25ii <93009091+drg25ii@users.noreply.github.com>
2026-02-20 17:24:21 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Porniți scraper-ul intern NodeJS Torrentio (ascultă pe portul 7000 intern)
-echo "Pornim Torrentio Scraper intern..."
-cd /scraper && node index.js &
-
-# Porniți Torrenthan
-echo "Pornim Torrenthan..."
-cd /app && uvicorn main:app --host 0.0.0.0 --port ${PORT:-7002}
